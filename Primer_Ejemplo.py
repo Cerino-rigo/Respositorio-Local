0cf7d9d7962447013cbeb618b71ce42710e34d0a
Rigo-cerino <cerino.rigoberto@tec.mx>
2025-10-01 16:25:40 -0600

    Agregué función suma

--- Primer_Ejemplo.py
+++ Primer_Ejemplo.py
@@ -2,5 +2,11 @@ print("Hola a todos, este es un repositorio local y se desplegará en Github")
 
 print("Este archivo se estará modificando")
 
-for i in range(10):
+for i in range(10): 
     print(i)
+
+
+def suma(a, b):
+    return a + b
+
+print("La suma de los números es: ", suma(89, 52))
